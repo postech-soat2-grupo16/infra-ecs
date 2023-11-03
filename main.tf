5e4b7cbce40f6d70fbff37dd9344cc9b1a9ee289
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-03 17:17:20 -0300

    fix sg ingress rule

--- main.tf
+++ main.tf
@@ -19,3 +19,13 @@ resource "aws_ecs_cluster" "cluster_ecs_soat" {
     infra = "cluster-ecs-soat"
   }
 }
+
+resource "aws_security_group_rule" "security_group_cluster_ecs_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = var.security_group_cluster_ecs
+  depends_on        = [aws_ecs_cluster.cluster_ecs_soat]
+}
